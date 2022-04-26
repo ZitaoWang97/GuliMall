2771afdafb2a763d7f229861157f0a658b6e7b39
Zitao Wang <zitao.wang@tum.de>
2022-04-26 13:31:19 +0200

    Project initial structure creation

---
+++ gulimall-coupon/src/test/java/com/zitao/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zitao.gulimall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gulimall-ware/src/test/java/com/zitao/gulimall/ware/GulimallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zitao.gulimall.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
